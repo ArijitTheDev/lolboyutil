a1ad225d4c4b5cab5a2b45380a662e31fa83dd81
Arijit Debnath <75007470+ArijitTheDev@users.noreply.github.com>
2022-03-30 15:27:39 +0530

    Update say.js

--- commands/mod/say.js
+++ commands/mod/say.js
@@ -7,6 +7,6 @@ module.exports = {
       },
 run: async (client, message, args, user, text, prefix) = {
 if (whitelist.id.includes(message.author.id)){
-message.channel.send(text)
+message.channel.send(text) }
 }
 }
