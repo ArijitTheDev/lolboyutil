6997a6badd6b2be254e952a136145aec0e901008
Arijit Debnath <75007470+ArijitTheDev@users.noreply.github.com>
2022-03-30 22:19:08 +0530

    Update say.js

--- commands/mod/say.js
+++ commands/mod/say.js
@@ -5,7 +5,7 @@ module.exports = {
            description: "Send a Message",
            aliases: ['s', 'bol']
       },
-run: async (client, message, args, user, text, prefix) = {
+run: async(client, message, args, user, text, prefix) = {
 if (whitelist.id.includes(message.author.id)){
 message.channel.send(text) }
 }
