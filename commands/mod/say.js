e270b633713440a630793df858ed74c993cf065f
Arijit Debnath <75007470+ArijitTheDev@users.noreply.github.com>
2022-03-31 21:42:43 +0530

    Update say.js

--- commands/mod/say.js
+++ commands/mod/say.js
@@ -1,4 +1,11 @@
-if (command === 'say') {
+module.exports = {
+    config: {
+        name: "say",
+        aliases: ["s", "bol"],
+        description: "Says the Message",
+        usage: "say <msg>",
+    },
+
 	let text = args.join(' ');
 	message.delete();
 	message.channel.send(text);
