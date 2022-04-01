3e1f0d502b34b96ff15dfd562110abdc95104896
Arijit Debnath <75007470+ArijitTheDev@users.noreply.github.com>
2022-04-01 14:35:57 +0530

    Update embed.js

--- commands/mod/embed.js
+++ commands/mod/embed.js
@@ -19,7 +19,7 @@ if (whitelist.id.includes(message.author.id)){
   const emsg = new Discord.MessageEmbed()
   .setTitle('Aurous')
   .setDescription(texto)
-  .setFooter('Made With 🤍 by Arijit#1000')
+  .setFooter('Aurous | Made With 🤍 by Arijit#1000')
 
 message.channel.send(emsg)
 message.delete();
