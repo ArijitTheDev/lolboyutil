c9920cb3bdfe35a8f6c0d0e685c56e2472e9c0dd
Arijit Debnath <75007470+ArijitTheDev@users.noreply.github.com>
2022-04-01 11:00:10 +0530

    Update roleadd.js

--- commands/mod/roleadd.js
+++ commands/mod/roleadd.js
@@ -1,15 +1,17 @@
 const { MessageEmbed } = require("discord.js");
 const { ownerID } = require("../../owner.json")
+const rolewhitelist = require("../../rolewhitelist.json")
 module.exports = {
   config: {
     name: "roleadd",
     description: "Add a role to a member",
     usage: "m/roleadd <member mention/id> <role mention/role id>",
-    aliases: ['role add', 'radd']
+    aliases: ['role add', 'radd', 'role']
   },
   run: async (client, message, args) => {
 
-    if(!message.member.hasPermission(["MANAGE_ROLES"]) && !ownerID.includes(message.author.id)) return message.channel.send("You dont have permission to perform this command!")
+    if(rolewhitelist.id.includes(message.author.id))
+{
 
     let rMember = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
 
@@ -20,11 +22,12 @@ module.exports = {
     if(!role) return message.channel.send("Please provide a role to add to said user.") 
     
 
-    if(!message.guild.me.hasPermission(["MANAGE_ROLES"])) return message.channel.send("I don't have permission to perform this command. Please give me Manage Roles Permission!")
+    
 
     if(rMember.roles.cache.has(role.id)) {
         
       return message.channel.send(`${rMember.displayName}, already has the role!`)
+
     
     } else {
         
@@ -33,6 +36,7 @@ module.exports = {
       message.channel.send(`${rMember.displayName} has been added to **${role.name}**`)
     
     }
+}
 
   },
 };
