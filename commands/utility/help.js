f2e4744682467a82427d8f7245fe551190601fd5
Arijit Debnath <75007470+ArijitTheDev@users.noreply.github.com>
2022-04-01 15:22:54 +0530

    Update help.js

--- commands/utility/help.js
+++ commands/utility/help.js
@@ -26,7 +26,7 @@ run: async (bot, message, args) => {
             console.log(e)
     };
 
-if(message.content.toLowerCase() === `${prefix}help`){
+/*if(message.content.toLowerCase() === `${prefix}help`){
     var log = new Discord.MessageEmbed()
     .setTitle("**Help Menu: Main**")
     .setColor(`#d9d9d9`)
@@ -34,8 +34,8 @@ if(message.content.toLowerCase() === `${prefix}help`){
     .addField(`**Utility**`, `[ \`${prefix}help utility\` ]`, true)
 
 message.channel.send(log);
-} 
-else if(args[0].toLowerCase() === "mod") {
+} */
+if(message.content.toLowerCase() === `${prefix}help`) {
     var commandArray = "1) Ban \n2) Unban\n3) Kick\n4) Mute\n5) Unmute\n6) Warn\n7) Purge\n8) Slowmode \n9) Nick (Needs Access)\n10) Setmuterole"
     var commandA2 = "11) Disablemuterole\n12) Setmodlog\n13) Disablemodlog\n14) Lock (Lock the channel)\n15) Unlock (Unlock the channel)\n16) Lockdown (Fully Lock the whole server. [FOR EMRGENCIES ONLY]) \n17) Hackban\\forceban <id> \n18) Setxp"
     var commandA3 = "19) Disablexp\n20) Dm (Needs Access)\n21) Say (Needs Access)\n22) Embed(Needs Access)\n23) Role (Needs Access)\n24) Rdel - Remove a role(Needs Access)\n25) Svr - Set Server Region"
